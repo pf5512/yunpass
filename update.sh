685de05ca6893b983fc36c77bf11c8ade8a672e7
kangbiao <kangbiao@kangbiao.org>
2015-11-03 20:40:51 +0800

    sads

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ read COMMIT
 mysqldump --add-drop-database=TRUE -uroot -pkb123456 PropertySystem > PropertySystem.sql
 echo "数据库已备份..."
 git add .
-git commit -m '$COMMIT'
+git commit -m $COMMIT
 echo "已提交到本地..."
 git push mine master
 echo "提交成功"
